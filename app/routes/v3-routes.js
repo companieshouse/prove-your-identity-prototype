ec6efd89d96c7822dcc8f0695b33e32f9d93282e
James Francis <jfranciswebdesign@users.noreply.github.com>
2023-05-22 13:21:02 +0100

    V3 created

---
+++ app/routes/v3-routes.js
@@ -0,0 +1,224 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+
+// ******* Sign in validation ********************************
+router.get('/v3/sign-in', function (req, res) {
+  // Set URl
+  res.render('v3/sign-in', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/sign-in', function (req, res) {
+  // Create empty array and set error variables to false
+  var errors = []
+  var emailHasError = false
+  var passwordHasError = false
+
+  // Check if user has filled out a email
+  if (req.session.data['emailAddress'] === '') {
+    // No value so add error to array
+    emailHasError = true
+    errors.push({
+      text: 'Enter your email address',
+      href: '#emailAddress'
+    })
+  }
+
+  // Check if user has filled out a password
+  if (req.session.data['password'] === '') {
+    // No value so add error to array
+    passwordHasError = true
+    errors.push({
+      text: 'Enter your password',
+      href: '#password'
+    })
+  }
+
+  // Check if eother filed not filled out
+  if (emailHasError || passwordHasError) {
+    // Re-show page with error value as true so errors will show
+    res.render('v3/sign-in', {
+      errorEmail: emailHasError,
+      errorPassword: passwordHasError,
+      errorList: errors
+    })
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v3/one-login-intro')
+  }
+})
+
+// ******* id-check javascript ********************************
+router.get('/v3/id-check', function (req, res) {
+  // Set URl
+  res.render('v3/id-check', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/id-check', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['has-ID'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'Select what type of ID you have',
+      href: '#has-ID'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v3/id-check', {
+      errorID: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['has-ID'] == 'no-id' ) {
+      res.redirect('/v3/acsp-alternate')
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v3/sign-in')
+    }
+  }
+})
+
+// ******* route javascript ********************************
+router.get('/v3/route', function (req, res) {
+  // Set URl
+  res.render('v3/route', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/route', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['which-route'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'You must select how you would you like to verify your identity with Companies House',
+      href: '#which-route'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v3/route', {
+      errorRoute: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['which-route'] == 'gov') {
+      res.redirect('/v3/resident')
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v3/acsp-route')
+    }
+  }
+})
+
+
+// ******* resident javascript ********************************
+router.get('/v3/resident', function (req, res) {
+  // Set URl
+  res.render('v3/resident', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/resident', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['is-resident'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'You must select if you have a UK driving licence',
+      href: '#is-resident'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v3/resident', {
+      errorResident: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['is-resident'] === 'yes') {
+      res.redirect('/v3/sign-in')
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v3/id-check')
+    }
+  }
+})
+
+
+// ******* one-login-start javascript ********************************
+router.get('/v3/one-login-start', function (req, res) {
+  // Set URl
+  res.render('v3/one-login-start', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/one-login-start', function (req, res) {
+  res.redirect('/v3/one-login-working')
+})
+
+// ******* one-login-working javascript ********************************
+router.get('/v3/one-login-working', function (req, res) {
+  // Set URl
+  res.render('v3/one-login-working', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/one-login-working', function (req, res) {
+  res.redirect('/v3/address-lookup')
+})
+
+
+// ******* address-lookup javascript ********************************
+router.get('/v3/address-lookup', function (req, res) {
+  // Set URl
+  res.render('v3/address-lookup', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/address-lookup', function (req, res) {
+  res.redirect('/v3/address-confirm')
+})
+
+// ******* address-confirm javascript ********************************
+router.get('/v3/address-confirm', function (req, res) {
+  // Set URl
+  res.render('v3/address-confirm', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/address-confirm', function (req, res) {
+  res.redirect('/v3/verified-success')
+})
+
+
+// ******* address-manual javascript ********************************
+router.get('/v3/address-manual', function (req, res) {
+  // Set URl
+  res.render('v3/address-manual', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/address-manual', function (req, res) {
+  res.redirect('/v3/address-confirm')
+})
+
+
+module.exports=router;
+
