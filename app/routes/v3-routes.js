07d5bfef9411c177a6c2ec899608a47e84bafe0a
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2023-05-31 14:36:33 +0100

    v3 changes

--- app/routes/v3-routes.js
+++ app/routes/v3-routes.js
@@ -46,7 +46,7 @@ router.post('/v3/sign-in', function (req, res) {
     })
   } else {
     // User inputted value so move to next page
-    res.redirect('/v3/one-login-intro')
+    res.redirect('/v3/resident')
   }
 })
 
@@ -80,10 +80,11 @@ router.post('/v3/id-check', function (req, res) {
       res.redirect('/v3/acsp-alternate')
     } else {
       // User inputted value so move to next page
-      res.redirect('/v3/sign-in')
+      res.redirect('/v3/one-login-intro')
     }
   }
 })
+
 
 // ******* route javascript ********************************
 router.get('/v3/route', function (req, res) {
@@ -112,7 +113,7 @@ router.post('/v3/route', function (req, res) {
     })
   } else {
     if (req.session.data['which-route'] == 'gov') {
-      res.redirect('/v3/resident')
+      res.redirect('/v3/sign-in')
     } else {
       // User inputted value so move to next page
       res.redirect('/v3/acsp-route')
@@ -137,7 +138,7 @@ router.post('/v3/resident', function (req, res) {
   if (typeof req.session.data['is-resident'] === 'undefined') {
     // No value so add error to array
     errors.push({
-      text: 'You must select if you have a UK driving licence',
+      text: 'You must select if you have a home address in the UK',
       href: '#is-resident'
     })
 
@@ -148,7 +149,7 @@ router.post('/v3/resident', function (req, res) {
     })
   } else {
     if (req.session.data['is-resident'] === 'yes') {
-      res.redirect('/v3/sign-in')
+      res.redirect('/v3/one-login-intro')
     } else {
       // User inputted value so move to next page
       res.redirect('/v3/id-check')
@@ -182,41 +183,142 @@ router.post('/v3/one-login-working', function (req, res) {
 })
 
 
-// ******* address-lookup javascript ********************************
-router.get('/v3/address-lookup', function (req, res) {
+// ******* psc-existing-company javascript ********************************
+router.get('/v3/psc-verification/psc-existing-company', function (req, res) {
   // Set URl
-  res.render('v3/address-lookup', {
+  res.render('v3/psc-verification/psc-existing-company', {
     currentUrl: req.originalUrl
   })
 })
 
-router.post('/v3/address-lookup', function (req, res) {
-  res.redirect('/v3/address-confirm')
+router.post('/v3/psc-verification/psc-existing-company', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['psc-existing'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'You must select if you are a person with significant control',
+      href: '#psc-existing'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v3/psc-verification/psc-existing-company', {
+      errorPsc: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['psc-existing'] === 'yes') {
+      res.redirect('/v3/psc-verification/company-number')
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v3/psc-verification/verified-success-psc')
+    }
+  }
 })
 
-// ******* address-confirm javascript ********************************
-router.get('/v3/address-confirm', function (req, res) {
+
+// ******* psc-statement javascript ********************************
+router.get('/v3/psc-verification/psc-statement', function (req, res) {
   // Set URl
-  res.render('v3/address-confirm', {
+  res.render('v3/psc-verification/psc-statement', {
     currentUrl: req.originalUrl
   })
 })
 
-router.post('/v3/address-confirm', function (req, res) {
-  res.redirect('/v3/verified-success')
+router.post('/v3/psc-verification/psc-statement', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['verification-Statement'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'You must confirm the verification statement',
+      href: '#verification-Statement'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v3/psc-verification/psc-statement', {
+      errorStatement: true,
+      errorList: errors
+    })
+  } else {
+    res.redirect('/v3/psc-verification/other-appointments')
+  }
 })
 
 
-// ******* address-manual javascript ********************************
-router.get('/v3/address-manual', function (req, res) {
+// ******* other-appointments javascript ********************************
+router.get('/v3/psc-verification/other-appointments', function (req, res) {
   // Set URl
-  res.render('v3/address-manual', {
+  res.render('v3/psc-verification/other-appointments', {
     currentUrl: req.originalUrl
   })
 })
 
-router.post('/v3/address-manual', function (req, res) {
-  res.redirect('/v3/address-confirm')
+router.post('/v3/psc-verification/other-appointments', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['psc-another'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'You must select if you are a PSC for another company',
+      href: '#psc-another'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v3/psc-verification/other-appointments', {
+      errorAnotherPsc: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['psc-another'] === 'yes') {
+      res.redirect('/v3/psc-verification/company-number')
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v3/psc-verification/check-your-answers')
+    }
+  }
+})
+
+
+// ******* appointments javascript ********************************
+router.get('/v3/psc-verification/appointments', function (req, res) {
+  // Set URl
+  res.render('v3/psc-verification/appointments', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/psc-verification/appointments', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['psc-choice'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'You must select which person with significant control you are',
+      href: '#psc-choice'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v3/psc-verification/appointments', {
+      errorPSC: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['psc-choice'] == 'sally') {
+      res.redirect('/v3/psc-verification/psc-statement')
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v3/psc-verification/psc-verified-failure')
+    }
+  }
 })
 
 
