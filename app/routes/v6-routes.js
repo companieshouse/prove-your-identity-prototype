e80535b6b42bfe3eb4bc8184c646a8653998960e
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2023-09-06 11:34:47 +0100

    one login sign in pages

--- app/routes/v6-routes.js
+++ app/routes/v6-routes.js
@@ -2,51 +2,74 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 
-// ******* Sign in validation ********************************
-router.get('/v6/pre-one-login/sign-in', function (req, res) {
+// ******* Sign in email validation ********************************
+router.get('/v6/pre-one-login/sign-in-email', function (req, res) {
   // Set URl
-  res.render('v6/pre-one-login/sign-in', {
+  res.render('v6/pre-one-login/sign-in-email', {
     currentUrl: req.originalUrl
   })
 })
 
-router.post('/v6/pre-one-login/sign-in', function (req, res) {
+router.post('/v6/pre-one-login/sign-in-email', function (req, res) {
   // Create empty array and set error variables to false
   var errors = []
-  var emailHasError = false
-  var passwordHasError = false
 
   // Check if user has filled out a email
-  if (req.session.data['emailAddress'] === '') {
+  if (req.session.data['signin-email'] === '') {
     // No value so add error to array
-    emailHasError = true
     errors.push({
       text: 'Enter your email address',
-      href: '#emailAddress'
+      href: '#signin-email'
     })
   }
 
-  // Check if user has filled out a password
-  if (req.session.data['password'] === '') {
-    // No value so add error to array
-    passwordHasError = true
-    errors.push({
-      text: 'Enter your password',
-      href: '#password'
-    })
-  }
 
   // Check if eother filed not filled out
-  if (emailHasError || passwordHasError) {
+  if (req.session.data['signin-email'] === '') {
     // Re-show page with error value as true so errors will show
-    res.render('v6/pre-one-login/sign-in', {
-      errorEmail: emailHasError,
-      errorPassword: passwordHasError,
+    res.render('v6/pre-one-login/sign-in-email', {
+      errorSigninEmail: true,
       errorList: errors
     })
   } else {
     // User inputted value so move to next page
-    res.redirect('/v6/pre-one-login/you-need-to-verify')
+    res.redirect('/v6/pre-one-login/sign-in-password')
+  }
+})
+
+
+// ******* Sign in password validation ********************************
+router.get('/v6/pre-one-login/sign-in-password', function (req, res) {
+  // Set URl
+  res.render('v6/pre-one-login/sign-in-password', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v6/pre-one-login/sign-in-password', function (req, res) {
+  // Create empty array and set error variables to false
+  var errors = []
+
+  // Check if user has filled out a email
+  if (req.session.data['signin-password'] === '') {
+    // No value so add error to array
+    errors.push({
+      text: 'Enter your password',
+      href: '#signin-password'
+    })
+  }
+
+
+  // Check if eother filed not filled out
+  if (req.session.data['signin-password'] === '') {
+    // Re-show page with error value as true so errors will show
+    res.render('v6/pre-one-login/sign-in-password', {
+      errorSigninPassword: true,
+      errorList: errors
+    })
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v6/pre-one-login/one-login-intro')
   }
 })
 
