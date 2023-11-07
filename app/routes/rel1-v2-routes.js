09937551664e1c02feb01e785871ac4734d8890b
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2023-11-07 12:05:12 +0000

    v2 changes

--- app/routes/rel1-v2-routes.js
+++ app/routes/rel1-v2-routes.js
@@ -2,6 +2,25 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 
+// ******* start validation ********************************
+router.get('/release1-v2/pre-one-login/start', function (req, res) {
+  // Set URl
+  res.render('release1-v2/pre-one-login/start', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/release1-v2/pre-one-login/start', function (req, res) {
+  if (req.session.data['version'] === 'sole-trader') {
+    // ACSP
+    res.redirect('/release1-v2/pre-one-login/one-login-intro')
+  } else {
+    // General
+    res.redirect('/release1-v2/pre-one-login/create-or-sign-in')
+  }
+})
+
+
 // ******* Sign in email validation ********************************
 router.get('/release1-v2/pre-one-login/sign-in-email', function (req, res) {
   // Set URl
@@ -69,22 +88,9 @@ router.post('/release1-v2/pre-one-login/sign-in-password', function (req, res) {
     })
   } else {
     // User inputted value so move to next page
-    res.redirect('/release1-v2/pre-one-login/you-need-to-verify')
+    res.redirect('/release1-v2/pre-one-login/one-login-intro')
   }
 })
-
-// ******* you-need-to-verify javascript ********************************
-router.get('/release1-v2/pre-one-login/you-need-to-verify', function (req, res) {
-  // Set URl
-  res.render('release1-v2/pre-one-login/you-need-to-verify', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/release1-v2/pre-one-login/you-need-to-verify', function (req, res) {
-  res.redirect('/release1-v2/pre-one-login/one-login-intro')
-})
-
 
 // ******* one-login-start javascript ********************************
 router.get('/release1-v2/pre-one-login/one-login-start', function (req, res) {
@@ -97,7 +103,6 @@ router.get('/release1-v2/pre-one-login/one-login-start', function (req, res) {
 router.post('/release1-v2/pre-one-login/one-login-start', function (req, res) {
   res.redirect('/release1-v2/one-login/one-login-working')
 })
-
 
 
 // ******* one-login/success validation ********************************
